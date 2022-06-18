2552e861716b09c1fff12b597a28d17364db469b
Shreeya Patel <shreeyapaatel@gmail.com>
2022-06-18 13:51:42 -0600

    initial commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -18,10 +18,11 @@ export default class About extends Component {
         </div>
         <div className="split right">
           <div className="centered">
-            <div className="name_title">Your Name</div>
+            <div className="name_title">Shreeya Patel</div>
             <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+            Hello, I am a rising sophomore studying Computer Science at 
+            Columbia University. I am from Northern Canada and my interests 
+            include hiking, reading, and watching Star Wars.
             </div>
           </div>
         </div>
